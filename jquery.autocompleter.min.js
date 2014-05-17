43b623e88374da05b0703b0429a992506da929bf
Artem Fitiskin <artem.fitiskin@yandex.ru>
2014-05-17 14:03:20 +0400

    create 0.1

--- jquery.autocompleter.min.js
+++ jquery.autocompleter.min.js
@@ -1,5 +1,5 @@
 /* 
- * Autocompleter v0.0.7 - 2014-05-17 
+ * Autocompleter v0.1 - 2014-05-17 
  * Simple, easy, customisable and with cache support. 
  * http://github.com/ArtemFitiskin/jquery-autocompleter 
  * 
